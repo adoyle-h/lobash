bc2da6773c541956a8d9aea94948a9890fa2f263
ADoyle <adoyle.h@gmail.com>
2019-06-21 20:01:31 +0800

    feat: add l.date && l.now_s use l.date

---
+++ src/modules/date.bash
@@ -0,0 +1,16 @@
+# ---
+# Category: Time
+# Since: 0.1.0
+# Usage: l.date [<format>=]
+# Description: Refer to 'man strftime' for <format>.
+# ---
+
+# Reference: https://github.com/dylanaraps/pure-bash-bible#get-the-current-date-using-strftime
+l.date() {
+  # %(datefmt)T : Causes printf to output the date-time string resulting from using datefmt as a
+  # format string for strftime(3). The corresponding argument is an integer representing the number
+  # of seconds since the epoch. Two special argument values may be used: -1 represents the
+  # current time, and -2 represents the time the shell was invoked. If no argument is specified,
+  # conversion behaves as if -1 had been given. This is an exception to the usual printf behavior.
+  printf "%($1)T\\n" "-1"
+}
--- src/modules/now_s.bash
+++ src/modules/now_s.bash
@@ -3,8 +3,9 @@
 # Since: 0.1.0
 # Usage: l.now_s
 # Description: Print the timestamp of the number of seconds that have elapsed since the Unix epoch (1 January 1970 00:00:00 UTC).
+# Dependent: date
 # ---
 
 l.now_s() {
-  date '+%s'
+  l.date '%s'
 }
