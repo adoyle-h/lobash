e0610dfb3ca69488998603071656c48ba32ccdc3
ADoyle <adoyle.h@gmail.com>
2019-06-27 19:42:02 +0800

    fix(now): to be compatible with Mac/Linux/BusyBox systems
    - Some Linux systems may not install the Perl module "Time::HiRes"

--- src/modules/now.bash
+++ src/modules/now.bash
@@ -5,10 +5,25 @@
 # Description: Print the timestamp of the number of milliseconds that have elapsed since the Unix epoch (1 January 1970 00:00:00 UTC).
 # ---
 
-l.now() {
-  # +%N not supported in MacOS.
-  # date '+%s%3N'
-
-  # Perl is ubiquitous.
+_l.perl_now() {
   perl -MTime::HiRes=time -e 'printf "%d\n", time * 1000'
+}
+
+l.now() {
+  local timestamp
+  if [[ $_LOBASH_OS == 'MacOS' ]]; then
+    # date '+%N' not supported in MacOS.
+    _l.perl_now
+  else
+    # Some Linux systems may not install the Perl module "Time::HiRes".
+    # So use date '+%3N' to get milliseconds.
+    timestamp=$(date '+%s%3N')
+
+    if [[ ${#timestamp} == 10 ]]; then
+      # But the date '+%N' is GNU date feature which not supported in Alpine/Busybox systems.
+      _l.perl_now
+    else
+      printf '%s\n' "$timestamp"
+    fi
+  fi
 }
