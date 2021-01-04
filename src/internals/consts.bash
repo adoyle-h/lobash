c29292ccd369cfbcaedefc4c2b23b368a33a7542
ADoyle <adoyle.h@gmail.com>
2021-01-04 15:23:35 +0800

    refactor(build): use sed to replace _PREFIX, _PUBLIC_DEPTH, _MIN_BASHVER insteadof overwrite them

--- src/internals/consts.bash
+++ src/internals/consts.bash
@@ -13,9 +13,10 @@ readonly _LOBASH_PRIVATE_FUNC_PREFIX=_l.
 readonly _LOBASH_PRIVATE_CONST_PREFIX=_L_
 readonly _LOBASH_PUBLIC_FUNC_PREFIX=l.
 readonly _LOBASH_PUBLIC_CONST_PREFIX=L_
-# _LOBASH_PREFIX will be reassigned when ./build
-_LOBASH_PREFIX=l.
-_LOBASH_PUBLIC_DEPTH=1
-_LOBASH_MIN_BASHVER=4.0
+
+# _LOBASH_PREFIX, _LOBASH_PUBLIC_DEPTH, _LOBASH_MIN_BASHVER will be reassigned when ./build
+readonly _LOBASH_PREFIX=l.
+readonly _LOBASH_PUBLIC_DEPTH=1
+readonly _LOBASH_MIN_BASHVER=4.0
 
 readonly _LOBASH_OS=$(_lobash.detect_os)
