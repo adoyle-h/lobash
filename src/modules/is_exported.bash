ab6e0448dc2063f4890f6c81fe9c170076d17441
ADoyle <adoyle.h@gmail.com>
2022-10-29 04:03:19 +0800

    fix: is_exported not work when bash < 5.0

--- src/modules/is_exported.bash
+++ src/modules/is_exported.bash
@@ -2,19 +2,20 @@
 # Category: Condition
 # Since: 0.5.0
 # Bash: 4.0
-# Dependent: is_undefined
 # Usage: l.is_exported <var_name>
 # Description: Check whether a shell variable is exported.
 # Description: Return 0 (true) or 1 (false). This function should never throw exception error.
 # ---
 
 l.is_exported() {
-  if l.is_undefined "$1"; then
-    return 1
-  fi
-
-  if [[ "${!1@a}" == *x* ]]; then
-    return 0
+  local s
+  if s=$(declare -p "$1" 2>/dev/null) ;then
+    s=$(echo "$s" | sed -E 's/^declare ([-a-z]+) /\1/')
+    if [[ $s == *x* ]]; then
+      return 0
+    else
+      return 1
+    fi
   else
     return 1
   fi
