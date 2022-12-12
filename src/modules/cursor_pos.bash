019aa5c1002b8a81123b81bae317de173b711a68
ADoyle <adoyle.h@gmail.com>
2022-12-13 00:03:13 +0800

    feat(l.cursor_pos): support variable parameter
    now it supports `l.cursor_pos <array_name>`

--- src/modules/cursor_pos.bash
+++ src/modules/cursor_pos.bash
@@ -1,9 +1,11 @@
 # ---
 # Category: Terminal
 # Since: 0.1.0
-# Usage: l.cursor_pos
-# Usage: IFS=';' c_pos=( $(l.cursor_pos) )
-# Description: Get current cursor position. It will print "row;column" with default IFS.
+# Usage: l.cursor_pos [<array_name>]
+# Usage: { IFS=';'; pos=( $(l.cursor_pos) ); }
+# Description: Get current cursor position.
+# Description: If `[<array_name>]` passed, it will assign row to `${array_name[0]}`, and col to `${array_name[1]}`.
+# Description: If `[<array_name>]` not passed, it will print "row;column".
 # ---
 
 # l.cursor_pos() {
@@ -15,5 +17,10 @@ l.cursor_pos() {
   local CURPOS
   read -rsdR -p $'\E[6n' CURPOS
   CURPOS=${CURPOS#*[} # Strip decoration characters <ESC>[
-  echo "${CURPOS}"    # Return position in "row;col" format
+
+  if (( $# > 0 )); then
+    IFS=';' read -ra "$1" <<<"${CURPOS}"
+  else
+    echo "${CURPOS}"    # Return position in "row;col" format
+  fi
 }
